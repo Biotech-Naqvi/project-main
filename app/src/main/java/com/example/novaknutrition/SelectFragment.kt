0ca65b607896e1d86ad33bcfa4ddbce34cb4c76d
Charles20199 <113481961+Charles20199@users.noreply.github.com>
2022-11-14 20:49:46 -0800

    novak

--- app/src/main/java/com/example/novaknutrition/SelectFragment.kt
+++ app/src/main/java/com/example/novaknutrition/SelectFragment.kt
@@ -21,19 +21,19 @@ class SelectFragment : Fragment() {
 
         FatMilk2.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "2 Percent Fat Milk: Serving Size = 1 cup,   11.98 carbs, 8.17g protein, 4.64 total fat, 0g fiber, 95.16mg sodium, 11.93g sugar, 122 Calories. Novak Rating 2/10")
+                putString("food", "2 Percent Fat Milk: Serving Size = 1 cup,   11.98 carbs, 8.17g protein, 4.64 total fat, 0g fiber, 95.16mg sodium, 11.93g sugar, 122 Calories. Novak Rating 7/10")
             })
         }
         val AlmondMilk: Button = view.findViewById(R.id.AlmondMilk)
         AlmondMilk.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Almond Milk: Serving Size = 1 cup    8g carbs, 1g protein, 2.5g total fat, 0.3g fiber, 150mg sodium, 7g sugar, 149 Calories, Novak Rating 2/10")
+                putString("food", "Almond Milk: Serving Size = 1 cup    8g carbs, 1g protein, 2.5g total fat, 0.3g fiber, 150mg sodium, 7g sugar, 149 Calories, Novak Rating 8/10")
             })
         }
         val AsianSlaw:Button = view.findViewById(R.id.AsianSlaw)
         AsianSlaw.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Asian Slaw With Dressing: Serving Size = 100g    20g carbs, 4g protein, 6g total fat, 5g fiber, 560mg sodium, 4g sugar, 116 Calories, Novak Rating 3/10")
+                putString("food", "Asian Slaw With Dressing: Serving Size = 100g    20g carbs, 4g protein, 6g total fat, 5g fiber, 560mg sodium, 4g sugar, 116 Calories, Novak Rating 7/10")
 
             })
         }
@@ -47,7 +47,7 @@ class SelectFragment : Fragment() {
         val CheeseBurger:Button = view.findViewById(R.id.ChesseBurger)
         CheeseBurger.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Cheeseburger: Serving Size = 103g    35g carbs, 17g protein, 14 grams total fat, 1.95g fiber, 630mg sodium, 6.01g sugar, 350 Calories, Novak Rating 9/10")
+                putString("food", "Cheeseburger: Serving Size = 103g    35g carbs, 17g protein, 14 grams total fat, 1.95g fiber, 630mg sodium, 6.01g sugar, 350 Calories, Novak Rating 1/10")
 
             })
         }
@@ -61,98 +61,98 @@ class SelectFragment : Fragment() {
         val Hawaiian:Button = view.findViewById(R.id.HawaiianRoles)
         Hawaiian.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Hawaiian Rolls: Serving Size = 1 Roll    16g carbs, 0g protein, 7 grams total fat, 0g fiber, 80g sodium, 8g sugar, 90 Calories, Novak Rating 4/10")
+                putString("food", "Hawaiian Rolls: Serving Size = 1 Roll    16g carbs, 0g protein, 7 grams total fat, 0g fiber, 80g sodium, 8g sugar, 90 Calories, Novak Rating 6/10")
 
             })
         }
         val HoneyApple:Button = view.findViewById(R.id.HoneyAppl)
         HoneyApple.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Honeycrisp Apple: Serving Size = 5.4 fl oz    22g carbs, 0 protein, 0 grams total fat, 5 fiber, 0mg sodium, 16 sugar, 80 Calories, Novak Rating 4/10")
+                putString("food", "Honeycrisp Apple: Serving Size = 5.4 fl oz    22g carbs, 0 protein, 0 grams total fat, 5 fiber, 0mg sodium, 16 sugar, 80 Calories, Novak Rating 6/10")
 
             })
         }
         val Horchata:Button = view.findViewById(R.id.Horchata)
         Horchata.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Horchata: Serving Size = 1 cup    26g carbs, 3g protein, 3g grams total fat, 0g fiber, 45mg sodium, 24g sugar, 140 Calories, Novak Rating 6/10")
+                putString("food", "Horchata: Serving Size = 1 cup    26g carbs, 3g protein, 3g grams total fat, 0g fiber, 45mg sodium, 24g sugar, 140 Calories, Novak Rating 4/10")
 
             })
         }
         val HotDog:Button = view.findViewById(R.id.HotDog)
         HotDog.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Hot Dog: Serving Size = 102g    13g carbs, 9g protein, 18.6 grams total fat, 0.8g fiber, 810mg sodium, 4g sugar, 314 Calories, Novak Rating 6/10")
+                putString("food", "Hot Dog: Serving Size = 102g    13g carbs, 9g protein, 18.6 grams total fat, 0.8g fiber, 810mg sodium, 4g sugar, 314 Calories, Novak Rating 4/10")
 
             })
         }
         val MeatRavi:Button = view.findViewById(R.id.MeatRavioli)
         MeatRavi.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Meat Ravioli: Serving Size = 1 cup    35.73 carbs, 21.82 protein, 15.55 grams total fat, 2.5g fiber, 1490mg sodium, 4.5 sugar, 373 Calories, Novak Rating 6/10")
+                putString("food", "Meat Ravioli: Serving Size = 1 cup    35.73 carbs, 21.82 protein, 15.55 grams total fat, 2.5g fiber, 1490mg sodium, 4.5 sugar, 373 Calories, Novak Rating 3/10")
 
             })
         }
         val OJ:Button = view.findViewById(R.id.OrangeJuice)
         OJ.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Orange Juice: Serving Size = 2.8 fl oz    8.9g carbs, 0.6g protein, 0.2 grams total fat, 0.2g fiber, 0mg sodium, 7.2g sugar, 39 Calories, Novak Rating 3/10")
+                putString("food", "Orange Juice: Serving Size = 2.8 fl oz    8.9g carbs, 0.6g protein, 0.2 grams total fat, 0.2g fiber, 0mg sodium, 7.2g sugar, 39 Calories, Novak Rating 1/10")
 
             })
         }
         val OL:Button = view.findViewById(R.id.OriginalLays)
         OL.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Lays Original: Serving Size = 15 chips    15g carbs, 2g protein, 10g grams total fat, 1g fiber, 170mg sodium, 0.5g sugar, 160 Calories, Novak Rating 3/10")
+                putString("food", "Lays Original: Serving Size = 15 chips    15g carbs, 2g protein, 10g grams total fat, 1g fiber, 170mg sodium, 0.5g sugar, 160 Calories, Novak Rating 4/10")
 
             })
         }
         val PB:Button = view.findViewById(R.id.PeanutButter)
         PB.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Peanut Butter: Serving Size = 32g    8g carbs, 7g protein, 16 grams total fat, 2g fiber, 136mg sodium, 3g sugar, 190 Calories, Novak Rating 3/10")
+                putString("food", "Peanut Butter: Serving Size = 32g    8g carbs, 7g protein, 16 grams total fat, 2g fiber, 136mg sodium, 3g sugar, 190 Calories, Novak Rating 7/10")
 
             })
         }
         val PS:Button = view.findViewById(R.id.PizzaSlice)
         PS.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Pizza Slice: Serving Size = 107g    35.6g carbs, 12.2g protein, 10.4 grams total fat, 2.5g fiber, 640mg sodium, 3.8g sugar, 285 Calories, Novak Rating 8/10")
+                putString("food", "Pizza Slice: Serving Size = 107g    35.6g carbs, 12.2g protein, 10.4 grams total fat, 2.5g fiber, 640mg sodium, 3.8g sugar, 285 Calories, Novak Rating 2/10")
 
             })
         }
         val PF:Button = view.findViewById(R.id.PotatoFries)
         PF.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Potato Fries: Serving Size = 3 oz    32.7g carbs, 3.2 protein, 13.7 grams total fat, 4.4 fiber, 247mg sodium, 0.6g sugar, 267 Calories, Novak Rating 10/10")
+                putString("food", "Potato Fries: Serving Size = 3 oz    32.7g carbs, 3.2 protein, 13.7 grams total fat, 4.4 fiber, 247mg sodium, 0.6g sugar, 267 Calories, Novak Rating 3/10")
 
             })
         }
         val RB:Button = view.findViewById(R.id.RawBroccoli)
         RB.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Raw Broccoli: Serving Size = 3.1oz    5.8 carbs, 2.5g protein, 0.3 grams total fat, 2.3 fiber, 29mg sodium, 1.5g sugar, 30 Calories, Novak Rating 1/10")
+                putString("food", "Broccoli: Serving Size = 3.1oz    5.8 carbs, 2.5g protein, 0.3 grams total fat, 2.3 fiber, 29mg sodium, 1.5g sugar, 30 Calories, Novak Rating 10/10")
 
             })
         }
         val Sea:Button = view.findViewById(R.id.SeafoodPaella)
         Sea.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Seafood Paella: Serving Size = 200g    37g carbs, 12g protein, 9 grams total fat, 2.3g fiber, 315mg sodium, 2.5g sugar, 215 Calories, Novak Rating 4/10")
+                putString("food", "Seafood Paella: Serving Size = 200g    37g carbs, 12g protein, 9 grams total fat, 2.3g fiber, 315mg sodium, 2.5g sugar, 215 Calories, Novak Rating 8/10")
 
             })
         }
         val SBread:Button = view.findViewById(R.id.SourdoughBread)
         SBread.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Sourdough Bread: Serving Size = 2 inch slice   18.1 carbs, 3.8g protein, 0.6 grams total fat, 0.8 fiber, 208mg sodium, 0.8 sugar, 92 Calories, Novak Rating 5/10")
+                putString("food", "Sourdough Bread: Serving Size = 2 inch slice   18.1 carbs, 3.8g protein, 0.6 grams total fat, 0.8 fiber, 208mg sodium, 0.8 sugar, 92 Calories, Novak Rating 3/10")
 
             })
         }
         val Spaghetti:Button = view.findViewById(R.id.Spaghetti)
         Spaghetti.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Plain Spaghetti: Serving Size = 3.7oz    41.1g carbs, 7.8g protein, 1.2g grams total fat, 2.4g fiber, 1mg sodium, 0.8 sugar, 212 Calories, Novak Rating 9/10")
+                putString("food", "Plain Spaghetti: Serving Size = 3.7oz    41.1g carbs, 7.8g protein, 1.2g grams total fat, 2.4g fiber, 1mg sodium, 0.8 sugar, 212 Calories, Novak Rating 4/10")
 
             })
         }
@@ -174,21 +174,21 @@ class SelectFragment : Fragment() {
         val WhiteCheddar:Button = view.findViewById(R.id.WhiteCheddar)
         WhiteCheddar.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "White Cheddar: Serving Size = 28g    0g carbs, 6g protein, 10g grams total fat, 0g fiber, 170mg sodium, 0g sugar, 120 Calories, Novak Rating 1/10")
+                putString("food", "White Cheddar: Serving Size = 28g    0g carbs, 6g protein, 10g grams total fat, 0g fiber, 170mg sodium, 0g sugar, 120 Calories, Novak Rating 10/10")
 
             })
         }
         val WhiteRice:Button = view.findViewById(R.id.WhiteRice)
         WhiteRice.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "White Rice: Serving Size = 5.6oz    44.5g carbs, 4.3 protein, 0.4 grams total fat, 0.6g fiber, 2mg sodium, 0 sugar, 205 Calories, Novak Rating 6/10")
+                putString("food", "White Rice: Serving Size = 5.6oz    44.5g carbs, 4.3 protein, 0.4 grams total fat, 0.6g fiber, 2mg sodium, 0 sugar, 205 Calories, Novak Rating 3/10")
 
             })
         }
         val WholeMilk:Button = view.findViewById(R.id.WholeMilk)
         WholeMilk.setOnClickListener{
             findNavController().navigate(R.id.SelectToDetails, Bundle().apply {
-                putString("food", "Whole Milk: Serving Size = 8fl oz    12.8g carbs, 7.9g protein, 8 grams total fat, 0 fiber, 98mg sodium, 12.8g sugar, 146 Calories, Novak Rating 2/10")
+                putString("food", "Whole Milk: Serving Size = 8fl oz    12.8g carbs, 7.9g protein, 8 grams total fat, 0 fiber, 98mg sodium, 12.8g sugar, 146 Calories, Novak Rating 6/10")
 
             })
         }
--- app/src/main/java/com/example/novaknutrition/SuggestFragment.kt
+++ app/src/main/java/com/example/novaknutrition/SuggestFragment.kt
@@ -22,14 +22,15 @@ class SuggestFragment : Fragment() {
         HomeFromSuggestButton.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.SuggestToFirst)
         }
-            val calculateButton: Button = view.findViewById(R.id.calculate)
-            calculateButton.setOnClickListener{
-                calculate()
-            }
+        val calculateButton: Button = view.findViewById(R.id.calculate)
+        calculateButton.setOnClickListener{
+            calculate()
+        }
 
 
 
     }
+
 
     fun calculate() {
 
@@ -37,52 +38,216 @@ class SuggestFragment : Fragment() {
         var suggestion: Int = 0
         val glucoseText = view?.findViewById<EditText>(R.id.glucose)
         var glucoseLevel = glucoseText?.text.toString()
-        var level = glucoseLevel.toInt()
+        var level = glucoseLevel.toDouble()
+        val foodlist = mutableListOf<String>("Orange Juice", "Pizza Slice", "Cheeseburger", "Meat Ravioli", "Potato Fries", "White Rice", "Hawaiian Rolls", "Horchata", "Hot Dog","Lays Original","Spaghetti","Tortilla Chips", "Brioche Bun", "Hard Boiled Egg","Sourdough Bread", "Honeycrisp Apple", "Whole Milk","2% Fat Milk","Asian Slaw","Peanut Butter","Thai Green Curry","Almond Milk","Seafood Paella","Raw Broccoli","White Cheddar")
+        var index1: Int
+        var index2: Int
+        var index3: Int
+        var index4: Int
+        var index5: Int
+
+
         if(level < 80){
+            index1 = (0..17).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (0..17).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (0..17).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (0..17).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (0..17).random()
+            }
+
+
+
             if (suggestionText != null) {
-                suggestionText.text = "Your glucose level is excellent and you don't need to take action to control it. Try eating some of these foods in your next meal: cheeseburger, brioche bun, meat ravioli, Thai Green Curry, Sourdough Bread"
+                suggestionText.text = "Your glucose level is excellent. Try eating some of these foods in your next meal: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
 
             }
 
         }
         else if(level >= 80 && level < 115){
+            index1 = (2..20).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (2..20).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (2..20).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (2..20).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (2..20).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: Spaghetti, Pizza, Asian Slaw, Orange Juice, Hawaiian Rolls, Hard Boiled Egg"
+                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 115 && level < 150){
+            index1 = (3..22).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (3..22).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (3..22).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (3..22).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (3..22).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: Hot Dog, White rice, Original Lays, Raw Brocolli, Almond Milk, Honeycrisp Apple"
+                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 150 && level < 180){
+            index1 = (6..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (6..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (6..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (6..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (6..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: Horchata, Brioche Bun, Tortilla chips, Asian slaw, whole milk, white rice, Peanut butter"
+                suggestionText.text = "Your glucose level is healthy. Try eating some of these foods in your next meal: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 180 && level < 215){
+            index1 = (9..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (9..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (9..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (9..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (9..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your Glucose level is normal for diabetes. Try eating some of these foods in your next meal to control your blood sugar: Sourhdough Bread, Orange Juice, Tortilla chips, White Cheddar, Almond Milk, Original Lays, Safeood Paella"
+                suggestionText.text = "Your Glucose level is normal for diabetes. Try eating some of these foods in your next meal to control your blood sugar: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 215 && level < 250){
+            index1 = (10..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (10..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (10..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (10..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (10..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your Glucose level is normal for diabetes. Try eating some of these foods in your next meal to control your blood sugar: Hard Boiled Egg, Brioche Bun, Asian slaw, Raw broccoli, 2% fat milk, Hawaiian Rolls"
+                suggestionText.text = "Your Glucose level is normal for diabetes. Try eating some of these foods in your next meal to control your blood sugar: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 250 && level < 280){
+            index1 = (12..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (12..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (12..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (12..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (12..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your Glucose level is high. Try eating some of these foods in your next meal to lower your blood sugar: Tortilla Chips, Honeycrisp apple, whole milk, white cheddar, raw broccoli, orange juice, peanut butter"
+                suggestionText.text = "Your Glucose level is high. Try eating some of these foods in your next meal to lower your blood sugar: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}"
             }
         }
         else if(level >= 280 && level < 315){
+            index1 = (15..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (15..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (15..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (15..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (15..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your Glucose level is high. Try eating some of these foods in your next meal to lower your blood sugar: Honeycrisp apple, hawaiian rolls, Asian slaw, almond milk, original lays, Tortilla chips"
+                suggestionText.text = "Your Glucose level is high. Try eating some of these foods in your next meal to lower your blood sugar: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}. You should also take a ketonetes test."
             }
         }
         else if(level >= 315){
+            index1 = (17..24).random()
+            index2= -1
+            index3= -1
+            index4 = -1
+            index5 = -1
+            while(index2 == index1 || index2 == -1){
+                index2 = (17..24).random()
+            }
+            while(index3 == index2 || index3 == index1 || index3 == -1){
+                index3 = (17..24).random()
+            }
+            while(index4 == index3 || index4 == index2 || index4 == index1 || index4 == -1){
+                index4 = (17..24).random()
+            }
+            while(index5 == index4 || index5 == index3 || index5 == index2 || index5 == index1 ||  index5 == -1){
+                index5 = (17..24).random()
+            }
             if (suggestionText != null) {
-                suggestionText.text = "Your Glucose level is very high. Try eating some of these foods in your next meal to lower your blood sugar: Original lays, orange juice, asian slaw, whole milk, raw broccoli, white cheddar"
+                suggestionText.text = "Your Glucose level is very high. Try eating some of these foods in your next meal to lower your blood sugar: ${foodlist.elementAt(index1)}, ${foodlist.elementAt(index2)}, ${foodlist.elementAt(index3)}, ${foodlist.elementAt(index4)}, ${foodlist.elementAt(index5)}. You should also take a ketonetes test."
             }
         }
 
